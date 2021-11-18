ca393e4c08584560806b65166f27952a729b3923
Emily Schwartau <emilyschwartau+1@gmail.com>
2021-11-17 18:03:33 -0600

    priority value issue request

--- src/components/AddTaskForm/AddTaskForm.jsx
+++ src/components/AddTaskForm/AddTaskForm.jsx
@@ -10,7 +10,8 @@ function AddTaskForm () {
 
     const userInputs = useSelector(store => store.userInputsReducer);
 
-    let [newTask, setNewTask] = useState({ task: '', due_date: '', importance: '', time_requirement: '', notes: '', priority_value: ''});
+    //let [newTask, setNewTask] = useState({ task: '', due_date: '', importance: '', time_requirement: '', notes: '', priority_value: ''});
+    let [newTask, setNewTask] = useState({ task: '', due_date: '', importance: '', time_requirement: '', notes: ''});
 
     //series of functions to add properties to local state object
     const handleTaskChange = (event) => {
@@ -33,9 +34,10 @@ function AddTaskForm () {
         setNewTask({...newTask, notes: event.target.value})
     }
 
-    //setNewTask({...newTask, priority_value: priorityValue});
+    
+    let priorityValue = 0;
 
-    const handlePriorityValueChange = (event) => {
+    const handlePriorityValue = () => {
         const current = new Date();
         const date = `${current.getMonth()+1}/${current.getDate()}/${current.getFullYear()}`;
 
@@ -53,53 +55,50 @@ function AddTaskForm () {
         let xValue = (userInputs.importance * .01) * (14);
         let yValue = (14)-DifferenceInDays;
 
-        let priorityValue = xValue * yValue;
+        priorityValue = xValue * yValue;
 
-        setNewTask({...newTask, priority_value: priorityValue})
-    }
+        //dispatch({type: "UPDATE_PRIORITY_VALUE", payload: Number(priorityValue)});
+
+        //setNewTask({...newTask, priority_value: priorityValue})
+        
+
+            return (
+                priorityValue
+                
+            )
+        }
+
+        handlePriorityValue();
+
+        //dispatch({type: "UPDATE_PRIORITY_VALUE", payload: Number(priorityValue)});
+    
+        //setNewTask({...newTask, priority_value: priorityValue})
+
 
     //function to dispatch an action to add local state object to DB
     //sets of chain of events to add to DB, re-get all list items from DB,
     //and update list item reducer
     const addNewTask = event => {
+        //handlePriorityValue();
+
         event.preventDefault();
         //dispatching to reducer that holds user inputs
         //for manipulation prior to sending to DB
         dispatch({ type: "SUBMIT_TASK_DETAILS", payload: newTask });
-        //dispatch({type: "UPDATE_PRIORITY_VALUE", payload: priorityValue});
+      
+        //handlePriorityValue();
+       
+        dispatch({type: "UPDATE_PRIORITY_VALUE", payload: Number(priorityValue)});
 
         //this was immediately adding to DB
         //dispatch({ type: 'ADD_TASK', payload: newTask });
     }
-        // function findPriorityValue() {
-        //         //generating today's date
-        //         const current = new Date();
-        //         const date = `${current.getMonth()+1}/${current.getDate()}/${current.getFullYear()}`;
-        
-        //         // To set two dates to two variables
-        //         //date1 is today's date, date2 is due date
-        //         let date1 = new Date(date);
-        //         let date2 = new Date(userInputs.due_date);
-              
-        //         // To calculate the time difference of two dates
-        //         let DifferenceInTime = date2.getTime() - date1.getTime();
-              
-        //         // To calculate the no. of days between two dates
-        //         let DifferenceInDays = DifferenceInTime / (1000 * 3600 * 24);
-        
-        //         let xValue = (userInputs.importance * .01) * (14);
-        //         let yValue = (14)-DifferenceInDays;
-        
-        //         let priorityValue = xValue * yValue;
+           
+    console.log(priorityValue);
 
-        //     return (
-        //         console.log(priorityValue)
-        //     )
-
-        // }
-
-        // findPriorityValue();
-
+    console.log(newTask);
+    
+    console.log(userInputs);
 
     return (
         <>
